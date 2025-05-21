95637c08e90cc1515db1330062fe2c2916e5ddad
mohammadarmin1390 <amirmmdi8686@gmail.com>
2025-05-21 10:34:59 +0330

    Add files via upload

---
+++ my-portfolio/script.js
@@ -0,0 +1,16 @@
+﻿// دکمه مود تاریک
+const toggleBtn = document.getElementById('darkModeToggle');
+toggleBtn.addEventListener('click', () => {
+  document.body.classList.toggle('dark-mode');
+});
+
+// انیمیشن اسکرول
+const faders = document.querySelectorAll('.fade-in');
+window.addEventListener('scroll', () => {
+  faders.forEach(el => {
+    const rect = el.getBoundingClientRect();
+    if (rect.top < window.innerHeight - 100) {
+      el.classList.add('show');
+    }
+  });
+});
